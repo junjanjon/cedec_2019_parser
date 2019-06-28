934835db5a00eeda0de6ab77a4d04dc8f01d0f28
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-06-29 01:40:39 +0900

    add script

---
+++ cedec.rb
@@ -0,0 +1,21 @@
+require 'nokogiri'
+
+def news
+  %x(curl -O https://cedec.cesa.or.jp/2019/session)
+  doc = Nokogiri::HTML(File.read('session'))
+  doc.xpath('//div[@class="col-sm-12 col-md-10 session-right"]').map do |node|
+    meta = node.xpath('.//div[@class="col-12 col-sm-5 col-md-7 meta"]').inner_text.gsub(/[\r ,]/, "").gsub(/\n+/, "/").split('/')
+    meta_info = meta[1..-2].join('/')
+
+    {
+      title: node.xpath('./div[@class="session-title"]').inner_text.gsub(/[\r\n ,]/, ""),
+      speaker: node.xpath('.//div[@class="speaker_info"]/div[@class="name"]').inner_text.gsub(/[\r ,]/, "").gsub(/\n+/, "/"),
+      meta: meta_info,
+      url: node.xpath('.//a[@class="d-inline-block btn-elinvar-detail"]').attribute("href").text
+    }
+  end
+end
+
+news.each do |n|
+  puts "#{n[:title]},#{n[:speaker]},#{n[:meta]},#{n[:url]}"
+end
